4522d55bd72107e1d7e493eab55fd849b6c70b4f
julian.irmer <julian.irmer@emp.de>
2021-11-18 16:43:15 +0100

    week1: add task

---
+++ week 1/main.js
@@ -0,0 +1,118 @@
+// Addiere a und b
+function add(a, b) {
+    return a + b;
+}
+// TESTS:
+// console.log(add(1, 2));
+// console.log(add(-1, 10));
+// #####################################################
+
+
+// Schreibe den ersten und letzten Buchstaben des Strings groß
+function makeItBig(string) {
+
+}
+// TESTS:
+// console.log(makeItBig('hello'));
+// console.log(makeItBig('sophia'));
+
+
+// #####################################################
+// Gebe die Quersumme der gegeben Zahl zurück
+function returnChecksum(input) {
+
+}
+// TESTS:
+// console.log(returnChecksum(123));
+// console.log(returnChecksum(100));
+// console.log(returnChecksum(001));
+// console.log(returnChecksum('abc'));
+// console.log(returnChecksum());
+// #####################################################
+
+
+// Bestimme die Summe der Zahlen von 1 bis 100
+function oneTo100() {
+    return sum;
+}
+// console.log(oneTo100());
+// #####################################################
+
+
+// Gebe all ungeraden Zahlen von 1 bis 100 in der Konsole aus
+function showUneven() {
+
+}
+// #####################################################
+
+
+// Gebe all geraden Zahlen von 1 bis 100 in der Konsole aus
+function showEven() {
+
+}
+// #####################################################
+
+
+// Gebe alle Zahlen bis 100 in der Konsole aus. Zahlen, die durch 3 teilbar sind,
+// sollen mit dem Wort 'foo', Zahlen, die durch 5 teilbar sind, sollen mit dem Wort
+// 'bar' ausgegeben werden. Zahlen, die durch 3 und 5 teilbar sind, sollen mit dem Wort
+// 'foobar' ausgegeben werden.
+function foobar() {
+
+}
+// #####################################################
+
+
+// Konvertiere Fahrenheit zu Celcius
+function convertFahrenheit(input) {
+    return celcius;
+}
+
+// TESTS:
+// console.log(convertFahrenheit(100));
+// console.log(convertFahrenheit(-10));
+// console.log(convertFahrenheit('a'));
+// #####################################################
+
+
+// Addiere alle Elemente der Arrays
+const array1 = [1, 10, -3, 4, 102, 13];
+const array2 = [13, 5, -100, 'a'];
+
+function addElements() {
+
+return sum;
+}
+// TESTS:
+// console.log(addElements(array1));
+// console.log(addElements(array2));
+// console.log(addElements(array3));
+// #####################################################
+
+
+// Schreibe eine Funktion, die ein Array erhält, welches nur Zahlen enthält,
+// und addiere nur die positiven(!) Zahlen.
+const arr1 = [-1, 4, 10, -5];
+const arr2 = [-1, -2, -4, 1];
+
+function sumPositives() {
+    return sum;
+}
+
+// TESTS:
+// console.log(sumPositives(arr1));
+// console.log(sumPositives(arr2));
+// #####################################################
+
+
+// Finde die größte Zahl im Array und gebe diese zurück
+const arr4 = [1, 2, 3, 10, 4, 5];
+const arr5 = [402, 3, 12, 531, 70, 8];
+
+function findMax() {
+    return max;
+}
+
+// TESTS:
+// console.log(findMax(arr4));
+// console.log(findMax(arr5));
